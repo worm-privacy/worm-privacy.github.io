79e58d752de338c9b5eae0d44dbe0d4198992477
Arman <arman.omidi.1994@gmail.com>
2025-11-10 18:07:10 +0330

    Render additional sections in Home component
    Uncommented several sections in the Home component to render them.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
-import { TokenomicsSection } from '@/components/landing';
+import { NetworkStats, TeamSection, RoadmapSection, WormVSBethSection, HowItWorks, TokenomicsSection, HeroSection, SloganSection } from '@/components/landing';
 import { Footer } from '@/components/landing/footer';
 
 export default function Home() {
   return (
     <>
       <main>
-        {/* <HeroSection /> */}
-        {/* <SloganSection /> */}
-        {/* <HowItWorks /> */}
-        {/* <WormVSBethSection /> */}
-        {/* <NetworkStats /> */}
+        <HeroSection />
+        <SloganSection />
+        <HowItWorks />
+        <WormVSBethSection />
+        <NetworkStats />
         <TokenomicsSection />
-        {/* <RoadmapSection /> */}
-        {/* <TeamSection /> */}
+        <RoadmapSection />
+        <TeamSection />
       </main>
       <Footer />
     </>
