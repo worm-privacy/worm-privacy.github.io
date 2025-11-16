905a0b1a2dda420e03b0b70c3a5b48251e07d141
arman94 <arman.omidi.1994@gmail.com>
2025-11-16 22:35:31 +0330

    fix: check hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import {
-  HeroSection,
   HowItWorks,
   NetworkStats,
   RoadmapSection,
@@ -16,7 +15,7 @@ export default function Home() {
   return (
     <SmoothScroll>
       <main className="flex size-full flex-col items-center justify-center gap-2.5 md:pt-22 lg:px-40">
-        <HeroSection />
+        {/* <HeroSection /> */}
         <SloganSection />
         <HowItWorks />
         <WormVSBethSection />
