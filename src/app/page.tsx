31143edbd646637820b75edebd7d7bbe235f85b4
arman94 <arman.omidi.1994@gmail.com>
2025-11-16 17:38:39 +0330

    fix: scroll motion spring values

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import { ScrollerMotion } from 'scroller-motion';
-
 import {
   HeroSection,
   HowItWorks,
@@ -15,7 +13,8 @@ import { Footer } from '@/components/landing/footer';
 
 export default function Home() {
   return (
-    <ScrollerMotion className="w-full!" spring={{ mass: 1.1, stiffness: 500, damping: 50 }}>
+    // <ScrollerMotion className="w-full!" spring={{ mass: 1.1, stiffness: 500, damping: 50 }}>
+    <>
       <main className="flex size-full flex-col items-center justify-center gap-2.5 md:pt-22 lg:px-40">
         <HeroSection />
         <SloganSection />
@@ -27,6 +26,7 @@ export default function Home() {
         <TeamSection />
       </main>
       <Footer />
-    </ScrollerMotion>
+    </>
+    // </ScrollerMotion>
   );
 }
