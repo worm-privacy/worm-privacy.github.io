d92574342d8ce99eb23a847aea11fc077125f1e7
arman94 <arman.omidi.1994@gmail.com>
2025-11-16 17:40:48 +0330

    fix: scroll motion spring values

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,12 @@ import {
   WormVSBethSection,
 } from '@/components/landing';
 import { Footer } from '@/components/landing/footer';
+import { ScrollArea } from '@/components/ui';
 
 export default function Home() {
   return (
     // <ScrollerMotion className="w-full!" spring={{ mass: 1.1, stiffness: 500, damping: 50 }}>
-    <>
+    <ScrollArea className="h-screen w-screen max-w-full">
       <main className="flex size-full flex-col items-center justify-center gap-2.5 md:pt-22 lg:px-40">
         <HeroSection />
         <SloganSection />
@@ -26,7 +27,7 @@ export default function Home() {
         <TeamSection />
       </main>
       <Footer />
-    </>
+    </ScrollArea>
     // </ScrollerMotion>
   );
 }
