07ec3ccb71c1f2359d64afd4f1bc5c2c63ec2752
arman94 <arman.omidi.1994@gmail.com>
2025-11-19 14:30:52 +0330

    fix: add filling to animated worm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import {
+  Footer,
   HeroSection,
   HowItWorks,
   NetworkStats,
@@ -9,13 +10,24 @@ import {
   TeamSection,
   WormVSBethSection,
 } from '@/components/landing';
-import { Footer } from '@/components/landing/footer';
 import { Preloader } from '@/components/ui';
 import { SmoothScroll } from '@/components/ui/smoth-scroll';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
   const [loaded, setLoaded] = useState(false);
+
+  function onContentLoaded() {
+    setLoaded(true);
+  }
+
+  useEffect(() => {
+    window.addEventListener('DOMContentLoaded', onContentLoaded);
+
+    return () => {
+      window.removeEventListener('DOMContentLoaded', onContentLoaded);
+    };
+  }, []);
 
   return !loaded ? (
     <Preloader setLoaded={setLoaded} />
