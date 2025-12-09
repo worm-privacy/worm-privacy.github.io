38bc15ffe6ac8c4c20d32f518fe65eff9960847a
Ali Ghahremani <alighahremani1377@gmail.com>
2025-12-09 17:07:29 +0330

    feat: topbar ui

--- src/app/tools/burn-eth/page.tsx
+++ src/app/tools/burn-eth/page.tsx
@@ -1,14 +1,14 @@
 'use client';
 
 import { Footer } from '@/components/landing';
+import TopBar from '@/components/tools/topbar';
 import { SmoothScroll } from '@/components/ui/smoth-scroll';
 
 export default function BurnETH() {
-  return  (
+  return (
     <SmoothScroll>
-      <main className="flex size-full flex-col items-center justify-center gap-2.5 md:pt-22 lg:px-40">
-        "Burn ETH"
-      </main>
+      <TopBar />
+      <main className="flex size-full flex-col items-center justify-center gap-2.5 md:pt-22 lg:px-40">"Burn ETH"</main>
       <Footer />
     </SmoothScroll>
   );
---
+++ src/components/tools/topbar.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { useState } from 'react';
+import { Icons } from '../ui/icons';
+
+const TopBar = () => {
+  let isConnect = useState(false);
+  const walletAddress = '0xf3a123456789abcdef0123456789abcdef51d';
+
+  const truncateAddress = (addr: string) => {
+    return addr ? `${addr.slice(0, 5)}...${addr.slice(-3)}` : '';
+  };
+
+  return (
+    <div className="pl flex items-center justify-between rounded-b-3xl bg-surface1 px-[255px] py-4">
+      {/* Logo */}
+      <div className="flex items-center space-x-2">
+        <Icons.logo className="h-16" />
+        <Icons.typedLogo className="block" />
+      </div>
+      {isConnect ? (
+        <button className="flex items-center rounded-lg  bg-brand px-4 py-3 font-satoshi text-sm text-[14px] font-medium text-black transition-colors duration-200">
+          Connect Wallet
+        </button>
+      ) : (
+        <button className="flex items-center rounded-lg border border-emerald-400 bg-gray-800 px-3 py-3 font-mono text-sm text-emerald-400 transition-colors duration-200 hover:bg-emerald-400 hover:text-gray-900">
+          {truncateAddress(walletAddress)}
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default TopBar;
