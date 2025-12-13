f9191c1c93b1f2802a0b732c45776042223ee8a8
Ali Ghahremani <alighahremani1377@gmail.com>
2025-12-13 15:22:33 +0330

    feat: adding TopBar and TabBar to all tools

--- src/app/tools/claim-worm/page.tsx
+++ src/app/tools/claim-worm/page.tsx
@@ -1,11 +1,15 @@
 'use client';
 
 import { Footer } from '@/components/landing';
+import TabBar from '@/components/tools/tabbar';
+import TopBar from '@/components/tools/topbar';
 import { SmoothScroll } from '@/components/ui/smoth-scroll';
 
 export default function ClaimWorm() {
-  return  (
+  return (
     <SmoothScroll>
+      <TopBar />
+      <TabBar />
       <main className="flex size-full flex-col items-center justify-center gap-2.5 md:pt-22 lg:px-40">
         "Claim Worm"
       </main>
