269d90582992d46b90c582acdc996101dfc428dc
Ali Ghahremani <alighahremani1377@gmail.com>
2026-01-25 19:17:23 +0330

    update: contract addresses to sepolia

--- src/lib/core/contracts/beth.ts
+++ src/lib/core/contracts/beth.ts
@@ -6,7 +6,7 @@ import { RapidsnarkOutput } from '../miner-api/proof-get-by-nullifier';
 import { WORMcontractAddress } from './worm';
 
 //TODO change this address
-export const BETHContractAddress = '0x1a0cb44C7DcB767DD6f819DA0Ae16485B57C4738';
+export const BETHContractAddress = '0x0AF06bBE75a98B0062E67D4f49442cf73fA17586';
 
 export namespace BETHContract {
   export const approve = async (
