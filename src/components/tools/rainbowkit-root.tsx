e70e4d530d9b5701f79b86c8cd80c32960c100a1
Ali Ghahremani <alighahremani1377@gmail.com>
2026-01-26 12:22:28 +0330

    fix: CORS error by updating sepolia RPC

--- src/components/tools/rainbowkit-root.tsx
+++ src/components/tools/rainbowkit-root.tsx
@@ -3,6 +3,7 @@
 import { darkTheme, getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import '@rainbow-me/rainbowkit/styles.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { defineChain } from 'viem';
 import { WagmiProvider } from 'wagmi';
 
 const RainbowKitRoot = ({
@@ -23,7 +24,37 @@ export default RainbowKitRoot;
 
 const queryClient = new QueryClient();
 
-import { sepolia } from 'viem/chains';
+// import { sepolia } from 'viem/chains';
+
+export const sepolia = /*#__PURE__*/ defineChain({
+  id: 11_155_111,
+  name: 'Sepolia',
+  nativeCurrency: { name: 'Sepolia Ether', symbol: 'ETH', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://api.zan.top/eth-sepolia'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Etherscan',
+      url: 'https://sepolia.etherscan.io',
+      apiUrl: 'https://api-sepolia.etherscan.io/api',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 751532,
+    },
+    ensUniversalResolver: {
+      address: '0xeeeeeeee14d718c2b47d9923deab1335e144eeee',
+      blockCreated: 8_928_790,
+    },
+  },
+  testnet: true,
+});
+
 const config = getDefaultConfig({
   appName: 'Worm',
   projectId: 'ff488caeeb8e5b10e03f4fe7ba2fda00',
