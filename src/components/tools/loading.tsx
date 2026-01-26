966cf29d5f95a7dd8727eb9481b86d727bd648e2
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2026-01-26 14:10:21 +0330

    fix: Some ui changes

--- src/components/tools/loading.tsx
+++ src/components/tools/loading.tsx
@@ -2,7 +2,7 @@ export default function LoadingComponent() {
   return (
     <div className="grow text-white">
       <div className="mb-6 text-[24px]">Hold on a sec</div>
-      <div className="text-[18px]">Job is currently being processed</div>
+      <div className="text-[18px]">Job is currently being processed...</div>
     </div>
   );
 }
