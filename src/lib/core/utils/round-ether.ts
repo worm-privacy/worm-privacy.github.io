b44249e32b8ff7f9f4337ee6d8e3443d974d83b8
Ali Ghahremani <alighahremani1377@gmail.com>
2026-01-27 18:39:00 +0330

    improve: better way to format ETH amounts

--- src/lib/core/utils/round-ether.ts
+++ src/lib/core/utils/round-ether.ts
@@ -1,4 +1,10 @@
 import { formatUnits } from 'ethers';
 
-export const roundEther = (amount: bigint, precision?: number): string =>
-  parseFloat(formatUnits(amount, 18)).toFixed(precision ?? 4);
+export const roundEther = (amount: bigint, precision?: number): string => {
+  const f = parseFloat(formatUnits(amount, 18));
+  return f < 1 ? f.toPrecision(1) : f.toFixed(precision ?? 4);
+};
+
+export const roundEtherF = (f: number, precision?: number): string => {
+  return f < 1 ? f.toPrecision(1) : f.toFixed(precision ?? 4);
+};
