561cee64f2d0df85caa16b111a9b4c175f4835f6
Ali Ghahremani <alighahremani1377@gmail.com>
2026-02-03 11:43:25 +0330

    fix: buy/sell footer bug

--- src/app/tools/buy-sell-worm/page.tsx
+++ src/app/tools/buy-sell-worm/page.tsx
@@ -8,12 +8,15 @@ import { SmoothScroll } from '@/components/ui/smoth-scroll';
 export default function BuySellWorm() {
   return (
     <SmoothScroll>
-      <TopBar />
-      <TabBar />
-      <main className="flex size-full flex-col items-center justify-center gap-2.5 md:pt-22 lg:px-40">
-        <div className="text-[30px] text-white">Coming Soon...</div>
-      </main>
-      <Footer />
+      <div className="flex h-svh grow flex-col">
+        <TopBar />
+        <TabBar />
+        <main className="flex size-full flex-col items-center justify-center gap-2.5 md:pt-22 lg:px-40">
+          <div className="text-[30px] text-white">Coming Soon...</div>
+        </main>
+        <div className="min-h-[105px] grow" />
+        <Footer />
+      </div>
     </SmoothScroll>
   );
 }
