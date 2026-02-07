46d1764ea8ff544d2070a84d449d1ba2c69a7aa0
Ali Ghahremani <alighahremani1377@gmail.com>
2026-02-07 17:24:39 +0330

    ui-fix: topar sizing and spacing

--- src/components/tools/topbar.tsx
+++ src/components/tools/topbar.tsx
@@ -2,37 +2,23 @@
 
 import '@rainbow-me/rainbowkit/styles.css';
 import Link from 'next/link';
-import { useState } from 'react';
 import { Icons } from '../ui/icons';
 import { CustomConnectWalletButton } from './custom-connect-wallet-button';
 
 const TopBar = () => {
-  let isConnect = useState(false);
-  const walletAddress = '0xf3a123456789abcdef0123456789abcdef51d';
-
-  const truncateAddress = (addr: string) => {
-    return addr ? `${addr.slice(0, 5)}...${addr.slice(-3)}` : '';
-  };
-
   return (
-    <div className="h-[72px] rounded-b-3xl border border-[rgba(var(--brand-rgb),0.24)] bg-surface1">
-      <div className="m-auto flex max-w-310 flex-row items-center">
-        {/* Logo */}
+    <div className="flex h-[72px] flex-row justify-center rounded-b-3xl border border-[rgba(var(--brand-rgb),0.24)] bg-surface1">
+      <div className="flex h-[72px] w-310 flex-row items-center px-5">
         <Link href="/">
-          <div className="flex items-center space-x-2">
-            <Icons.logo className="h-16" />
+          <div className="flex h-10 items-center gap-2">
+            <Icons.logo className="h-10" />
             <Icons.typedLogo className="block" />
           </div>
         </Link>
 
         <div className="grow" />
-        {isConnect ? (
-          <CustomConnectWalletButton />
-        ) : (
-          <button className="flex items-center rounded-lg border border-emerald-400 bg-gray-800 px-3 py-3 font-mono text-sm text-emerald-400 transition-colors duration-200 hover:bg-emerald-400 hover:text-gray-900">
-            {truncateAddress(walletAddress)}
-          </button>
-        )}
+
+        <CustomConnectWalletButton />
       </div>
     </div>
   );
