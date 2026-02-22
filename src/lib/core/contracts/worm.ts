74f41f2fec238946b351c9b4d97b3e6e19dc8ea3
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2026-02-22 15:15:05 +0330

    Remove TWORM addr

--- src/lib/core/contracts/worm.ts
+++ src/lib/core/contracts/worm.ts
@@ -7,7 +7,6 @@ import { StakingContractAddress } from './staking';
 
 //TODO change this address
 // sepolia 0x0d2e09d2abf22ed938fadaa306ccd48329e09774
-// mainnet WORM 0x0d2e09d2abf22ed938fadaa306ccd48329e09774
 export const WORMcontractAddress = '0xfC9d98CdB3529F32cD7fb02d175547641e145B29';
 
 export namespace WORMContract {
