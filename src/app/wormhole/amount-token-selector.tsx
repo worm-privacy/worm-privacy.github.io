459d0f4f90b26ae5fa685cd108f8febad90c3306
Ali Ghahremani <alighahremani1377@gmail.com>
2026-04-29 14:19:57 +0330

    feat(wormhole): dollar estimate on inputs

--- src/app/wormhole/amount-token-selector.tsx
+++ src/app/wormhole/amount-token-selector.tsx
@@ -1,11 +1,39 @@
+import { useDebounceEffect } from '@/hooks/use-debounce-effect';
+import { UniswapV3Quoter } from '@/lib/core/contracts/uniswap/v3qouter';
+import { getListedTokenConfig, USDT } from '@/lib/core/tokens-config';
 import { useState } from 'react';
+import { formatUnits, parseUnits } from 'viem';
+import { useClient } from 'wagmi';
 import { InputComponentProps, SelectTokenDialog } from './token-selection-dialog';
 
 export const AmountTokenSelector = (props: InputComponentProps) => {
-  const receiveToken = props.tokenSelectionState.value;
+  const [dollarValue, setDollarValue] = useState('0');
+  const client = useClient();
 
-  // TODO estimate dollar on input change (with debounce)
-  const [dollarValue, setDollarValue] = useState(0n);
+  useDebounceEffect(
+    async () => {
+      try {
+        console.log(
+          `estimate type: ${props.typeName}, amount: ${props.amountState.value}, token: ${props.tokenSelectionState.value}`
+        );
+        if (props.amountState.value === '') throw 'empty input';
+        setDollarValue('...');
+        const path = props.tokenSelectionState.value!.pathToTether;
+        if (path.length === 0) return setDollarValue(props.amountState.value); // tokens is dollar stablecoin
+        const estimatedAmount = await UniswapV3Quoter.quoteExactInput(
+          client!,
+          path,
+          parseUnits(props.amountState.value, props.tokenSelectionState.value!.decimals)
+        );
+        setDollarValue(formatUnits(estimatedAmount, getListedTokenConfig(USDT)!.decimals));
+      } catch (e) {
+        console.error(e);
+        setDollarValue('?');
+      }
+    },
+    1000,
+    [props.amountState, props.tokenSelectionState]
+  );
 
   return (
     <div className="flex h-25 flex-row items-center rounded-lg bg-[rgba(var(--neutral-low-rgb),0.12)] px-3 hover:bg-[rgba(var(--neutral-low-rgb),0.24)]">
--- src/lib/core/tokens-config.ts
+++ src/lib/core/tokens-config.ts
@@ -4,42 +4,54 @@ export type ListedToken = {
   name: string;
   symbol: string;
   decimals: number;
-
   address: `0x${string}`;
-
   // address, fee, address, fee, address...
   pathToWETH: SwapPathType;
+  pathToTether: SwapPathType; // empty array if token is dollar stablecoin
 };
 
 export type SwapPathType = (number | `0x${string}`)[];
+
+export const DAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F' as const;
+export const USDT = '0xdac17f958d2ee523a2206206994597c13d831ec7' as const;
+export const USDC = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48' as const;
+export const WBTC = '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599' as const;
 
 export const LISTED_TOKENS: ListedToken[] = [
   {
     name: 'Dai Stablecoin',
     symbol: 'DAI',
     decimals: 18,
-    address: `0x6B175474E89094C44Da98b954EedeAC495271d0F`,
-    pathToWETH: ['0x6B175474E89094C44Da98b954EedeAC495271d0F', 3000, WETHContractAddress],
+    address: DAI,
+    pathToWETH: [DAI, 3000, WETHContractAddress],
+    pathToTether: [],
   },
   {
     name: 'USD Tether',
     symbol: 'USDT',
     decimals: 6,
-    address: `0xdac17f958d2ee523a2206206994597c13d831ec7`,
-    pathToWETH: ['0xdac17f958d2ee523a2206206994597c13d831ec7', 100, WETHContractAddress],
+    address: USDT,
+    pathToWETH: [USDT, 100, WETHContractAddress],
+    pathToTether: [],
   },
   {
     name: 'USD Coin',
     symbol: 'USDC',
     decimals: 6,
-    address: `0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48`,
-    pathToWETH: ['0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 500, WETHContractAddress],
+    address: USDC,
+    pathToWETH: [USDC, 500, WETHContractAddress],
+    pathToTether: [],
   },
   {
     name: 'Wrapped Bitcoin',
     symbol: 'WBTC',
     decimals: 8,
-    address: `0x2260fac5e5542a773aa44fbcfedf7c193bc2c599`,
-    pathToWETH: ['0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', 500, WETHContractAddress],
+    address: WBTC,
+    pathToWETH: [WBTC, 500, WETHContractAddress],
+    pathToTether: [WBTC, 500, USDT],
   },
 ];
+
+export const getListedTokenConfig = (address: `0x${string}`) => {
+  return LISTED_TOKENS.find((e) => e.address === address);
+};
