cce8e2b9b97c68a815c988723befe66a13f7f70e
Ali Ghahremani <alighahremani1377@gmail.com>
2026-05-13 13:11:18 +0330

    feat(wormhole): tooltips for fees (const details)

--- src/app/wormhole/cost-details.tsx
+++ src/app/wormhole/cost-details.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import InfoIconTooltip from '@/components/tools/info-icon-tooltip';
 import { CypherETHQuoterContract } from '@/lib/core/contracts/cyphereth-quoter';
 import { roundEther } from '@/lib/core/utils/round-ether';
 import { useEffect, useState } from 'react';
@@ -52,27 +53,42 @@ export default function WormholeCostDetailsComponent(props: {
         </svg>
       </button>
       <div
-        className={`overflow-hidden text-[#93A2B7] transition-all duration-300 ${isExpanded ? 'max-h-64 opacity-100' : 'max-h-0 opacity-0'}`}
+        className={`text-[#93A2B7] transition-all duration-300 ${isExpanded ? 'max-h-64 opacity-100' : 'max-h-0 opacity-0'}`}
       >
-        <div className="flex items-center justify-between py-1">
-          <span className="text-sm">Prover fee:</span>
+        <div className="flex flex-row py-1">
+          <span className="mr-[-4] text-sm">Prover fee</span>
+          <InfoIconTooltip content="Worm server fee to generate a zero knowledge proof of burn for you" />
+          <span className="ml-1 text-sm">:</span>
+          <div className="grow" />
           <span className="text-sm ">{proverFee} BETH</span>
         </div>
         <div className="flex items-center justify-between py-1">
-          <span className="text-sm">Broadcaster fee:</span>
+          <span className="mr-[-4] text-sm">Broadcaster fee</span>
+          <InfoIconTooltip content="Worm server fee to submit your proof to chain" />
+          <span className="ml-1 text-sm">:</span>
+          <div className="grow" />
           <span className="text-sm">{broadcasterFee} BETH</span>
         </div>
         <div className="flex items-center justify-between py-1">
-          <span className="text-sm">Protocol fee:</span>
+          <span className="mr-[-4] text-sm">Protocol fee</span>
+          <InfoIconTooltip content="0.5% fee which goes to Worm token stakers" />
+          <span className="ml-1 text-sm">:</span>
+          <div className="grow" />
           <span className="text-sm">{protocolFee} BETH</span>
         </div>
         {/* TODO add swap fee */}
         <div className="flex items-center justify-between py-1">
-          <span className="text-sm">BETH to ETH ratio:</span>
+          <span className="mr-[-4] text-sm">BETH to ETH ratio</span>
+          <InfoIconTooltip content="We swap Burned Ether to Ether for you and thats the ratio" />
+          <span className="ml-1 text-sm">:</span>
+          <div className="grow" />
           <span className="text-sm">{ratio}</span>
         </div>
         <div className="flex items-center justify-between py-1">
-          <span className="text-sm">Slippage tolerance:</span>
+          <span className="mr-[-4] text-sm">Slippage tolerance</span>
+          <InfoIconTooltip content="Slippage tolerance protects you from unexpected price changes while swapping" />
+          <span className="ml-1 text-sm">:</span>
+          <div className="grow" />
           <span className="text-sm">5%</span>
         </div>
       </div>
